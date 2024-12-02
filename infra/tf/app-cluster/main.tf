86c4ed10cf21dbd6805c86a34b6071298ec57b77
adam j <a.jemielita1@gmail.com>
2024-12-02 18:45:06 +0100

    db

--- infra/tf/app-cluster/main.tf
+++ infra/tf/app-cluster/main.tf
@@ -77,7 +77,6 @@ resource "azurerm_subnet" "aks_subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.1.1.0/24"]
-
 }
 
 # Subnet for PostgreSQL
