03d16f05843171e83818a4ccd1a62e06cc45a9b2
adam j <a.jemielita1@gmail.com>
2024-12-02 13:37:04 +0100

    db

--- infra/tf/app-cluster/main.tf
+++ infra/tf/app-cluster/main.tf
@@ -59,8 +59,9 @@ data "azurerm_container_registry" "current" {
 }
 
 # Resource Group
-data "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
+  location = var.location
 }
 
 # Virtual Network
