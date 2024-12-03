fe4f397f45f59186f7ea1819307f4e7e7e4f38e3
adam j <a.jemielita1@gmail.com>
2024-12-03 12:20:10 +0100

    db

--- infra/tf/app-cluster/main.tf
+++ infra/tf/app-cluster/main.tf
@@ -107,6 +107,8 @@ resource "azurerm_private_dns_zone_virtual_network_link" "postgres_dns_link" {
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.postgres_dns_zone.name
   virtual_network_id    = azurerm_virtual_network.example.id
+
+  depends_on = [ azurerm_virtual_network.example ]
 }
 
 # AKS Cluster
